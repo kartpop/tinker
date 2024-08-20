188ee4477d7f740103b64c012309870df4af294e
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-08-20 16:31:52 +0530

    chore: layout knogra and argonk

--- fe/web/src/App.tsx
+++ fe/web/src/App.tsx
@@ -1,8 +1,16 @@
+import Argonk from "./components/Argonk";
+import Knogra from "./components/Knogra";
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold ">Hello world!</h1>
-    </>
+    <div className="flex h-full">
+      <div className="w-2/3 bg-orange-400 h-screen">
+        <Knogra />
+      </div>
+      <div className="w-1/3 bg-orange-200 h-screen">
+        <Argonk />
+      </div>
+    </div>
   );
 }
 
---
+++ fe/web/src/components/Argonk.tsx
@@ -0,0 +1,7 @@
+export default function Argonk() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold text-slate-600 m-1 p-2">Argonk</h1>
+    </>
+  );
+}
