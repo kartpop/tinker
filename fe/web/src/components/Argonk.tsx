a04a8ed1ad610d080cc0dc5d5a56244c86de5a94
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-08-21 17:49:30 +0530

    feat(fe): setup websocket

--- fe/web/src/components/Argonk.tsx
+++ fe/web/src/components/Argonk.tsx
@@ -1,15 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Argonk() {
-  const [inputValue, setInputValue] = useState("");
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+  const [prompt, setPrompt] = useState("");
+  // const [messages, setMessages] = useState([]);
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8080/ws");
+
+    ws.onopen = () => {
+      console.log("WebSocket connection opened");
+    };
+
+    ws.onmessage = (event) => {
+      console.log("Received data:", event.data);
+      // setMessages((prevMessages) => [...prevMessages, event.data]);
+    };
+
+    ws.onclose = () => {
+      console.log("WebSocket connection closed");
+    };
+
+    setSocket(ws);
+
+    return () => {
+      ws.close();
+    };
+  }, []);
+
+  const handlePromptChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
     const textarea = event.target;
-    // Reset the height
     textarea.style.height = "auto";
-    // Adjust height based on scrollHeight
     textarea.style.height = `${textarea.scrollHeight}px`;
-    setInputValue(textarea.value);
+    setPrompt(textarea.value);
+  };
+
+  const handleSendPrompt = () => {
+    if (socket && prompt !== "") {
+      socket.send(prompt);
+      setPrompt("");
+    }
   };
 
   return (
@@ -68,16 +100,17 @@ export default function Argonk() {
       <div className="flex-shrink-0 flex items-center">
         <textarea
           rows={1}
-          value={inputValue}
-          onChange={handleInputChange}
+          value={prompt}
+          onChange={handlePromptChange}
           className="w-full p-2 border-2 bg-orange-50 border-gray-300 focus:border-orange-500 focus:outline-none resize-none h-auto max-h-36 overflow-y-auto"
           placeholder="Type a message..."
           style={{ height: "auto" }}
         />
         <img
           src="/up-arrow-png-20.png"
-          alt="Send message"
+          alt="Send prompt"
           className="w-9 h-9 ml-2 cursor-pointer"
+          onClick={handleSendPrompt}
         />
       </div>
     </div>
