8e2976aca1d78396c33d24ed413bb6da259a393f
KartikeyaPophali <kartikeyapophali@gmail.com>
2024-08-23 19:47:30 +0530

    feat: add google map in knogra

--- fe/web/src/components/Knogra.tsx
+++ fe/web/src/components/Knogra.tsx
@@ -1,7 +1,20 @@
+"use client";
+
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
+
 export default function Knogra() {
+  const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+  const position = { lat: 12.9716, lng: 77.5946 };
+
+  if (!apiKey) {
+    return <div>Error: Google Maps API key is missing</div>;
+  }
+
   return (
-    <>
-      <h1 className="text-2xl font-bold text-slate-600 m-1 p-2">Knogra</h1>
-    </>
+    <APIProvider apiKey={apiKey}>
+      <div className="h-screen">
+        <Map center={position} zoom={9} />
+      </div>
+    </APIProvider>
   );
 }
